581375b57e5a468fa8183b6b145a87b9d3ebb51e
EhsanKinux <ehsankhwentao@gmail.com>
2026-05-03 19:34:44 +0330

    Validating and Transforming Data from the URL

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,6 +9,7 @@ import {
 import { TasksService } from './tasks.service';
 import type { ITask } from './task.model';
 import { CreateTaskDto } from './create-task.dto';
+import { FindOneParams } from './find-one.params';
 
 @Controller('tasks')
 export class TasksController {
@@ -20,9 +21,9 @@ export class TasksController {
   }
 
   @Get('/:id')
-  public findOne(@Param('id') id: string): ITask {
-    const task = this.tasksService.findOne(id);
-
+  public findOne(@Param() params: FindOneParams): ITask {
+    const task = this.tasksService.findOne(params.id);
+    
     if (task) {
       return task;
     }
